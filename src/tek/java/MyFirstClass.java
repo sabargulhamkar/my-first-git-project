630aaf83d6a9992749d25886cc5344529f00dc81
sabargulhamkar <146789308+sabargulhamkar@users.noreply.github.com>
2023-10-05 22:35:58 -0500

    Update MyFirstClass.java

--- src/tek/java/MyFirstClass.java
+++ src/tek/java/MyFirstClass.java
@@ -3,5 +3,8 @@ package tek.java;
 public class MyFirstClass {
     public static void main(String[] args) {
         System.out.println("This is new changes");
+
+        system.out.println("Added by Sabar");
+            
     }
 }
